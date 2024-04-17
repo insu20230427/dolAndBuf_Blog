3a1b8e4d65f8f613195416daa97e7c677e98f509
insu <insubono@gmail.com>
2024-04-17 16:18:12 +0900

    <feature> config - SecurityConfig : 체인을 사용할 예정이므로 @EnableWebSecurity를 담, authorizeHttpRequests 변경

--- src/main/java/com/insu/blog/config/SecurityConfig.java
+++ src/main/java/com/insu/blog/config/SecurityConfig.java
@@ -5,18 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity // 필터를 필터 체인(Config)에 등록
 public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean // 인증 매니저에게 세션에 저장된 UserDetailsService와 PasswordEncoder를 자동 설정
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -29,15 +30,23 @@ public class SecurityConfig {
         // CSRF 비활성화
         http.csrf(AbstractHttpConfigurer::disable);
 
+//        http.sessionManagement(sessionManagement -> sessionManagement
+//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//                .sessionFixation().none()
+//                .maximumSessions(1).maxSessionsPreventsLogin(true)
+//        );
+
+
         http.authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests
-                                .requestMatchers("/", "/js/**", "/css/**", "/image/**").permitAll()
-                                .requestMatchers("/auth/**").permitAll()
-                                .requestMatchers("/api/**").permitAll()
-                                .requestMatchers("/WEB-INF/**").permitAll()
-                                .requestMatchers("/view/auth/**").permitAll()
-                                .anyRequest().authenticated() // 그 외 요청은 인증 필요
-                );
+                authorizeHttpRequests
+                        .requestMatchers("/manager/**").hasAnyRole("Role_ADMIN", "Role_MANAGER")
+                        .requestMatchers("/admin/**").hasRole("Role_ADMIN")
+                        .requestMatchers("/view/posts/**, /view/users/**").authenticated()
+                        .requestMatchers("/api/users/**, /api/posts/**, /api/replys/**").authenticated()
+                        .anyRequest().permitAll() // 그 외 요청은 인증 필요
+
+        );
+
 
 //      로그인폼을 리다이렉트 페이지 설정
         http.formLogin(formLogin ->
