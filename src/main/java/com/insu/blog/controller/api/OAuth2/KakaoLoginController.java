c8732d02218dcac976b8ceb7e147a27260a4818b
insu <insubono@gmail.com>
2024-05-03 14:27:24 +0900

    <feature> controller - OAuth2 관련 Controller : @RequestParam "code" 명시

--- src/main/java/com/insu/blog/controller/api/OAuth2/KakaoLoginController.java
+++ src/main/java/com/insu/blog/controller/api/OAuth2/KakaoLoginController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -14,7 +15,7 @@ public class KakaoLoginController {
 
     private final KakaoService kakaoService;
     @GetMapping("/api/oauth2/kakao/callback")
-    public String kakaoLogin(String code) throws JsonProcessingException {
+    public String kakaoLogin(@RequestParam("code") String code) throws JsonProcessingException {
         kakaoService.processKakaoUser(code);
         return "redirect:/";
     }
--- src/main/java/com/insu/blog/controller/api/OAuth2/NaverLoginController.java
+++ src/main/java/com/insu/blog/controller/api/OAuth2/NaverLoginController.java
@@ -5,6 +5,7 @@ import com.insu.blog.service.NaverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,7 +13,7 @@ public class NaverLoginController {
 
     private final NaverService naverService;
     @GetMapping("/api/oauth2/naver/callback")
-    public String naver(String code) throws JsonProcessingException {
+    public String naver(@RequestParam("code") String code) throws JsonProcessingException {
         naverService.processNaverUser(code);
         return "redirect:/";
     }
