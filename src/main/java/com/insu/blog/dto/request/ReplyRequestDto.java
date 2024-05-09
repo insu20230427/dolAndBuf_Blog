73a4b6f54b26aadb155ed066ceb2d73eb260c4ea
insu <insubono@gmail.com>
2024-05-09 15:28:34 +0900

    <feature> dto ReplyRequestDto update

--- src/main/java/com/insu/blog/dto/request/ReplyRequestDto.java
+++ src/main/java/com/insu/blog/dto/request/ReplyRequestDto.java
@@ -1,10 +1,8 @@
 package com.insu.blog.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ReplyRequestDto {
     private int userId;
     private int postId;
