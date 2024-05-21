9a54c21dffe4d0ad83f7d053f29d1c5eb0833a33
insu <insubono@gmail.com>
2024-05-22 00:44:23 +0900

    <feature> service update service

--- src/main/java/com/insu/blog/service/chat/ChatRoomService.java
+++ src/main/java/com/insu/blog/service/chat/ChatRoomService.java
@@ -3,6 +3,7 @@ package com.insu.blog.service.chat;
 import com.insu.blog.dto.chat.ChatMessageDto;
 import com.insu.blog.dto.chatRoom.*;
 import com.insu.blog.entity.User;
+import com.insu.blog.entity.chat.ChatMessage;
 import com.insu.blog.entity.chat.ChatRoom;
 import com.insu.blog.entity.chat.ChatUser;
 import com.insu.blog.repository.UserRepository;
@@ -18,7 +19,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Slf4j(topic = "ChatRoomService")
@@ -31,45 +34,48 @@ public class ChatRoomService {
     private final ChatMessageRepository chatMessageRepository;
     private static final int MESSAGE_PAGE_SIZE = 35;
 
-
+    // 채팅방 생성
     @Transactional
     public ChatRoomInfoResponseDto createChatRoom(User user, CreateChatRoomRequestDto requestDto) {
         ChatRoom newChatRoom = new ChatRoom(user, requestDto.getChatRoomName());
 
-        // userList에 초대한 사용자와 생성한 사용자 추가
-        List<User> userList = dtoToUserList(requestDto.getMemberIdList());
+        // userList에 초대한 사용자 추가
+        Set<User> userList = new HashSet<>(dtoToUserList(requestDto.getMemberIdList()));
         userList.add(user);
+
+        chatRoomRepository.save(newChatRoom);
 
         // 사용자와 상대방을 채팅방에 초대
         for (User u : userList) {
-            chatUserRepository.save(new ChatUser(u, newChatRoom));
+            ChatUser chatUser = new ChatUser(u, newChatRoom);
+            if (!chatUserRepository.existsByChatRoomAndUser(newChatRoom, u)) {
+                chatUserRepository.save(chatUser);
+            }
         }
 
-        chatRoomRepository.save(newChatRoom);
         return new ChatRoomInfoResponseDto(newChatRoom);
     }
 
 
-    public List<ChatMessageDto> getChatRoomChatMessages(int roomId, int page, User user) {
-        ChatRoom chatRoom = findChatRoomById(roomId);
+    // 채팅방 채팅 기록 조회
+    public List<ChatMessageDto> getChatRoomChatMessages(String roomId, User user) {
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(roomId));
         ChatUser chatUser = findChatUserByChatRoomAndUser(chatRoom, user);
 
-        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
-        Pageable pageable = PageRequest.of(page, MESSAGE_PAGE_SIZE, sort);
+        // 입장한 후의 모든 메세지를 조회할 수 있도록 페이지 관련 설정을 제거
+        List<ChatMessage> chatMessages = chatMessageRepository
+                .findAllByChatRoomIdAndCreatedAtGreaterThanEqual(Integer.parseInt(roomId), chatUser.getCreatedAt());
 
-        // 입장한 후의 메세지만 조회할 수 있도록
-        return chatMessageRepository
-                .findAllByChatRoomIdAndCreatedAtGreaterThanEqual(pageable, roomId, chatUser.getCreatedAt())
-                .stream()
+        return chatMessages.stream()
                 .map(ChatMessageDto::new)
                 .toList();
     }
 
-
+    // 채팅방 이름 수정
     @Transactional
-    public ChatRoomInfoResponseDto updateChatRoomName(int chatRoomId, User user, ChatRoomNameRequestDto requestDto) {
+    public ChatRoomInfoResponseDto updateChatRoomName(String chatRoomId, User user, ChatRoomNameRequestDto requestDto) {
         // 요청한 user가 채팅방의 생성자인지 확인
-        ChatRoom chatRoom = findChatRoomById(chatRoomId);
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(chatRoomId));
 
         if (!(chatRoom.getHostUserId() == user.getId())) {
             throw new IllegalArgumentException("채팅방의 이름은 호스트만 변경할 수 있습니다.");
@@ -79,10 +85,10 @@ public class ChatRoomService {
         return new ChatRoomInfoResponseDto(chatRoom);
     }
 
-
-    public void deleteChatRoom(int chatRoomId, User user) {
+    // 채팅방 삭제
+    public void deleteChatRoom(String chatRoomId, User user) {
         // 요청한 user가 채팅방의 생성자인지 확인
-        ChatRoom chatRoom = findChatRoomById(chatRoomId);
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(chatRoomId));
 
         if (!(chatRoom.getHostUserId() == user.getId())) {
             throw new IllegalArgumentException("채팅방은 호스트만 삭제할 수 있습니다.");
@@ -90,30 +96,34 @@ public class ChatRoomService {
         chatRoomRepository.delete(chatRoom);
     }
 
+    // 채팅방 멤버 조회
+    public MemberInfoListDto getChatRoomMembers(String chatRoomId) {
 
-    public MemberInfoListDto getChatRoomMembers(int chatRoomId) {
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(chatRoomId));
 
-        ChatRoom chatRoom = findChatRoomById(chatRoomId);
-
-        List<ChatMemberInfoDto> chatMemberInfoList = chatUserRepository.findAllByChatRoom(chatRoom)
-                .stream().map(ChatMemberInfoDto::new).toList();
+        List<ChatMemberInfoDto> chatMemberInfoList = chatUserRepository
+                .findAllByChatRoom(chatRoom)
+                .stream()
+                .map(ChatMemberInfoDto::new).toList();
 
         return new MemberInfoListDto(chatMemberInfoList);
     }
 
-
+     // 사용자가 속한 채팅방 전체 조회
     public MyChatRoomResponseDto getMyChatRooms(User user) {
-        List<ChatRoomInfoResponseDto> myChatRoomList = chatUserRepository.findAllByUser(user)
+        List<ChatRoomInfoResponseDto> myChatRoomList = chatUserRepository
+                .findAllByUser(user)
                 .stream()
                 .map(ChatUser::getChatRoom)
                 .map(ChatRoomInfoResponseDto::new).toList();
         return new MyChatRoomResponseDto(myChatRoomList);
     }
 
-
-    public void leaveChatRoom(int roomId, User user) {
-        ChatRoom chatRoom = findChatRoomById(roomId);
+     // 채팅방 나가기 
+    public void leaveChatRoom(String roomId, User user) {
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(roomId));
         ChatUser chatUser = findChatUserByChatRoomAndUser(chatRoom, user);
+
 
         if (chatUser == null) {
             throw new IllegalArgumentException("해당 채팅방에 속해있지 않습니다.");
@@ -124,19 +134,24 @@ public class ChatRoomService {
             deleteChatRoom(roomId, user);
         }
 
+        // user가 해당 채팅방의 host가 아니면 방만 나가기
         chatUserRepository.delete(chatUser);
     }
 
-
-    public void inviteMember(int roomId, MemberIdListDto memberIdListDto, User user) {
-        ChatRoom chatRoom = findChatRoomById(roomId);
+    // 해당 채팅방에 멤버 초대 
+    public void inviteMember(String roomId, MemberIdListDto memberIdListDto, User user) {
+        ChatRoom chatRoom = findChatRoomById(Integer.parseInt(roomId));
         ChatUser chatUser = findChatUserByChatRoomAndUser(chatRoom, user);
 
         if (chatUser == null) {
             throw new IllegalArgumentException("해당 채팅방으로의 초대 권한이 없습니다.");
         }
 
-        List<User> userList = dtoToUserList(memberIdListDto.getMemberIdList());
+        List<User> userList = new ArrayList<>();
+        for (ChatMemberIdDto cmd : memberIdListDto.getMemberIdList()) {
+            User newUser = findUserById(Integer.parseInt(cmd.getUserId()));
+            userList.add(newUser);
+        }
         for (User u : userList) {
             // 만약 이미 채팅방에 존재하는 멤버라면 건너뛰기
             if (findChatUserByChatRoomAndUser(chatRoom, u) != null) {
@@ -144,6 +159,18 @@ public class ChatRoomService {
             }
             chatUserRepository.save(new ChatUser(u, chatRoom));
         }
+    }
+
+    // 존재하는 사용자인지 조회
+    public List<User> dtoToUserList(List<ChatMemberIdDto> memberIdList) {
+        log.info(memberIdList.get(0).toString());
+        // 전달받은 사용자 리스트
+        List<User> userList = new ArrayList<>();
+        for (ChatMemberIdDto cmd : memberIdList) {
+            User user = findUserById(Integer.parseInt(cmd.getUserId()));
+            userList.add(user);
+        }
+        return userList;
     }
 
 
@@ -163,18 +190,6 @@ public class ChatRoomService {
 
     public ChatUser findChatUserByChatRoomAndUser(ChatRoom chatRoom, User user) {
         return chatUserRepository.findByChatRoomAndUser(chatRoom, user);
-    }
-
-
-    public List<User> dtoToUserList(List<ChatMemberIdDto> memberIdList) {
-        log.info(memberIdList.get(0).toString());
-        // 전달받은 사용자 리스트
-        List<User> userList = new ArrayList<>();
-        for (ChatMemberIdDto cmd : memberIdList) {
-            User user = findUserById(cmd.getUserId());
-            userList.add(user);
-        }
-        return userList;
     }
 }
 
--- src/main/java/com/insu/blog/service/chat/ChatService.java
+++ src/main/java/com/insu/blog/service/chat/ChatService.java
@@ -1,5 +1,6 @@
 package com.insu.blog.service.chat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.insu.blog.dto.chat.ChatMessageDto;
 import com.insu.blog.entity.User;
 import com.insu.blog.entity.chat.ChatMessage;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,29 +24,22 @@ public class ChatService {
 
     private final JwtUtil jwtUtil;
     private final ChannelTopic channelTopic;
-    private final RedisTemplate<String, String> redisTemplate;
+    // private final RedisTemplate<String, String> redisTemplate;
+	private final SimpMessagingTemplate template;
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
     private final ChatMessageRepository chatMessageRepository;
 
-    public String getRoomId(String destination) {
-        log.info("getRoomId 원래 destination -> " + destination);
-        int lastIndex = destination.lastIndexOf('/');
-        if (lastIndex != -1)
-            return destination.substring(lastIndex + 1);
-        else
-            return "";
-    }
-
-
     // 채팅방에 메시지 발송
-    public void sendChatMessage(ChatMessageDto chatMessageDto, String token) {
+    public void publishMessageToRedis(ChatMessageDto chatMessageDto, String token) {
         log.info("sendMessage 메서드 시작");
 
         // 메세지 header 의 토큰으로 sender 설정
-        String username = jwtUtil.getUserInfoFromToken(token).getSubject();
+        String pureToken = jwtUtil.substringToken(token);
+        String username = jwtUtil.getUserInfoFromToken(pureToken).getSubject();
         User user = findUserByUsername(username);
-
-        chatMessageDto.setSender(user.getNickname());
+//        chatMessageDto.setSender(user.getNickname());
+        chatMessageDto.setSender(username);
 
         // 나가기 메세지일 경우
         if (ChatMessageDto.MessageType.QUIT.equals(chatMessageDto.getType())) {
@@ -57,33 +52,52 @@ public class ChatService {
 
         // 메세지 dto에 현재 시각 설정 (사용자에게 바로 보내질 시각)
         chatMessageDto.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")));
-        log.info("메세지 발송 시각? " + chatMessageDto.getCreatedAt());
+        log.info("메세지 발송 시각? {}", chatMessageDto.getCreatedAt());
 
-        // 발송
-        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessageDto);
-    }
-
-    // 채팅방에 파일 발송
-    public void sendImageMessage(Long roomId, User user) {
-        log.info("sendImageMessage 메서드 시작");
-
-        ChatMessageDto chatMessageDto = ChatMessageDto.builder().roomId(roomId).build();
-        chatMessageDto.setSender(user.getNickname());
-
-        // 메세지 저장
-        chatMessageRepository.save(new ChatMessage(user, chatMessageDto));
-
-        // 메세지 dto에 현재 시각 설정 (사용자에게 바로 보내질 시각)
-        chatMessageDto.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")));
-        log.info("메세지 발송 시각? " + chatMessageDto.getCreatedAt());
-
-        // 발송
-        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessageDto);
+        // 메시지를 JSON 문자열로 변환하여 Redis로 발송
+        try {
+            String messageJson = objectMapper.writeValueAsString(chatMessageDto);
+            log.info("try 실행 전 ");
+            // redisTemplate.convertAndSend(channelTopic.getTopic(), messageJson);
+            template.convertAndSend("/topic/rooms/" + chatMessageDto.getRoomId(), messageJson);
+            log.info("try 실행 후 완료");
+        } catch (Exception e) {
+            log.error("Failed to publish message to Redis", e);
+        }
     }
 
     public User findUserByUsername(String username) {
         return userRepository.findByUsername(username).orElseThrow(() ->
                 new NullPointerException("존재하지 않는 사용자"));
     }
+
+    
+    // public String getRoomId(String destination) {
+    //     log.info("getRoomId 원래 destination -> " + destination);
+    //     int lastIndex = destination.lastIndexOf('/');
+    //     if (lastIndex != -1)
+    //         return destination.substring(lastIndex + 1);
+    //     else
+    //         return "";
+    // }
+
+        // // 채팅방에 파일 발송
+    // public void sendImageMessage(Long roomId, User user) {
+    //     log.info("sendImageMessage 메서드 시작");
+
+    //     ChatMessageDto chatMessageDto = ChatMessageDto.builder().roomId(roomId).build();
+    //     chatMessageDto.setSender(user.getNickname());
+
+    //     // 메세지 저장
+    //     chatMessageRepository.save(new ChatMessage(user, chatMessageDto));
+
+    //     // 메세지 dto에 현재 시각 설정 (사용자에게 바로 보내질 시각)
+    //     chatMessageDto.setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")));
+    //     log.info("메세지 발송 시각? " + chatMessageDto.getCreatedAt());
+
+    //     // 발송
+    //     redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessageDto);
+    // }
+
 }
 
