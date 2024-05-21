dfb61aa2aa54470e50712550f26003c1399d33e8
insu <insubono@gmail.com>
2024-05-22 00:42:09 +0900

    <feature> controller update controller

--- src/main/java/com/insu/blog/controller/api/chat/ChatController.java
+++ src/main/java/com/insu/blog/controller/api/chat/ChatController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import java.io.IOException;
 /*
     publisher 구현 (WebSocketChatHandler -> ChatController)
  */
+
+@SuppressWarnings("unused")
 @RequiredArgsConstructor
 @Controller
 @Slf4j(topic = "ChatController")
@@ -25,27 +28,25 @@ public class ChatController {
     private final ChatService chatService;
 //    private final S3Uploader s3Uploader;
 
-    /*
-        Websocket "/pub/chat/message"로 들어오는 메세지 처리
-     */
-    @MessageMapping("/chat/message")
+   
+    @MessageMapping("/chat/{roomId}/send")
     public void message(ChatMessageDto message, @Header("Authorization") String token) {
         log.info("ChatController - message 전송");
 
-        // Websocket 에 발행된 메세지 redis 로 발행 (publish)
-        chatService.sendChatMessage(message, token);
+         // 메시지를 Redis로 직접 발행
+        chatService.publishMessageToRedis(message, token);
     }
 
-    @PostMapping("/chat/file/{roomId}")
-    public void messageFile(
-            @PathVariable Long roomId,
-//            @RequestPart(value = "chatImage", required = false) MultipartFile chatImage,
-            @AuthenticationPrincipal PrincipalDetails principalDetails
-            ) {
-//        // s3 에 업로드
-//        String imageUrl = s3Uploader.upload(chatImage, "chat-images");
+//     @PostMapping("/chat/file/{roomId}")
+//     public void messageFile(
+//             @PathVariable Long roomId,
+// //            @RequestPart(value = "chatImage", required = false) MultipartFile chatImage,
+//             @AuthenticationPrincipal PrincipalDetails principalDetails
+//             ) {
+// //        // s3 에 업로드
+// //        String imageUrl = s3Uploader.upload(chatImage, "chat-images");
 
-//        chatService.sendImageMessage(roomId, imageUrl, userDetails.getUser());
-        chatService.sendImageMessage(roomId, principalDetails.getUser());
-    }
+// //        chatService.sendImageMessage(roomId, imageUrl, userDetails.getUser());
+//         chatService.sendImageMessage(roomId, principalDetails.getUser());
+//     }
 }
