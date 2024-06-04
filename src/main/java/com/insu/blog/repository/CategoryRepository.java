21c90e80fb1eaafe4a1356b5761cc2b381405ddf
최정욱 <choijw994@gmail.com>
2024-06-04 17:59:51 +0900

    <feature> back Repository category @Param 추가

--- src/main/java/com/insu/blog/repository/CategoryRepository.java
+++ src/main/java/com/insu/blog/repository/CategoryRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.insu.blog.entity.Category;
@@ -13,7 +14,7 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     // 추가적인 쿼리 메소드를 여기에 정의할 수 있습니다
 
     @Query("SELECT c FROM Category c WHERE c.user.id = :userId")
-    List<Category> findByUserId(int userId);
+    List<Category> findByUserId(@Param("userId") int userId);
 
     Category findByName(String name);
 }
