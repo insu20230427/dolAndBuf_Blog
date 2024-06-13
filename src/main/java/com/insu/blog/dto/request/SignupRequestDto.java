fd242338a3f6bc921fe7f6fa780886915cd28a81
insu <insubono@gmail.com>
2024-06-13 17:21:08 +0900

    <feature> dto update commit

--- src/main/java/com/insu/blog/dto/request/SignupRequestDto.java
+++ src/main/java/com/insu/blog/dto/request/SignupRequestDto.java
@@ -23,4 +23,6 @@ public class SignupRequestDto {
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    private String avatarImage;
 }
